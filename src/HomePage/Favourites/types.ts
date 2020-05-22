108827fc163e4bcbcfea3b4e264183470d79989e
Rie <rkaneko@javeriana.edu.co>
2020-05-22 11:54:48 -0500

    rebase

--- src/HomePage/Favourites/types.ts
+++ src/HomePage/Favourites/types.ts
@@ -1,5 +1,10 @@
 import { SingleMovieProp } from "../../shared/SingleMovie/types";
 
+<<<<<<< HEAD
 export interface FavouritesProps {
     favouritesData: Array<SingleMovieProp>;
+=======
+export interface FavoritesProps {
+    favoritesData: Array<SingleMovie>;
+>>>>>>> befea38... Added Homepage fan favorites
 }
