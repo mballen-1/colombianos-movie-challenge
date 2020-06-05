1b60915b7aa2d1227ea0258a72fdab6b8e4f829f
Rie Kaneko <rkaneko@mcclatchyservices.com>
2020-06-04 22:53:44 -0500

    added pagination funcionallity

--- src/Results/Filters/Filters.tsx
+++ src/Results/Filters/Filters.tsx
@@ -28,7 +28,9 @@ function Filters(props: FiltersProps) {
 
   return (
     <div className="filters-container">
+      <div>
       <h6 className="filter-h6">Display options:</h6>
+      </div>
       <Sorts sortsData={sortProps}></Sorts>
     </div>
   )
